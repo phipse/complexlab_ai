66ce7f735f6f52751efbf3651de456aeb8c22914
Philipp Eppelt <philipp88@eppelt.de>
2013-01-11 22:25:05 +0100

    can build valid request urls

--- src/crawlers/__init__.py
+++ src/crawlers/__init__.py
@@ -1,32 +1,50 @@
 from datetime import date
+from os import path
 
 class API_crawler:
   index = 0
-  
+  identifier = list()  
+  first = True
+  requestAddresses = list()
+
+  def __init__(self): 
+    self.initIdentifier()
+ 
+
+  def initIdentifier(self):
+    if self.first:
+      symfile = file("NASDAQ_syms", "r")
+      for line in symfile.readlines():
+	self.identifier.append(line.split("\n")[0])
+      self.first = False
+
 
   def setTimeFrame(self):
     startString = "&a=0&b=1&c=1900"
     today = date.today()
     pieces = str(today).split("-")
-#    print pieces
     startString += "&d="+ str( int(pieces[1])-1 );
     startString += "&e=" + str( int( pieces[2] ) );
     startString += "&f=" + str( int( pieces[0] ) );
     return startString
-#    print startString
+
 
   def setID(self):
-#    identifier = list(iterable)
-#    ret = identifier(self.index)
-#    self.index = self.index+1
-    ret = "s=GOOG"
+    ret = self.identifier[self.index]
+    self.index = self.index+1
+    if self.index > len(self.identifier):
+      self.index = 0
     return ret
 
   def run(self):
     startString = "http://ichart.finance.yahoo.com/table.csv?"
-    startString += self.setID();
-    startString += self.setTimeFrame()
-    print startString
+    for x in range(len(self.identifier)):
+      buildString = startString + "s=" + self.setID();
+      buildString += self.setTimeFrame()
+      self.requestAddresses.append(buildString)
+#    print len(self.requestAddresses)
+
+
 
 if __name__ == "__main__":
   crawl = API_crawler()
