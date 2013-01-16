c19280538a23a50114cf43fc5d50dcc5408a47fd
Philipp Eppelt <philipp88@eppelt.de>
2013-01-16 16:47:42 +0100

    grouper thoughts stub

---
+++ src/grouper/__init__.py
@@ -0,0 +1,41 @@
+
+
+
+
+
+
+
+class Grouper:
+  first = True
+
+  def init(self):
+    # open DB connection
+    # test connection to DB
+  
+  def grouping(self):
+  # while there is data:
+  # aquire dataset from the DB 
+  # for data in dataset: extract the featureset
+  # if featuresetgroup doesn't exist
+  #   open new group
+  # else:
+  #   get group
+  # add data to group
+
+  def cleanup(self):
+    # if group has not enough members 
+    #	mark as uninteresting
+
+  def run(self):
+    if first: 
+      init()
+      first = False
+
+
+
+
+
+if __name__ == "__main__":
+  grouper = Grouper()
+  grouper.run()
+  
