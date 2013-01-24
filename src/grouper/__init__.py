71549d0ae895aa9854f285a2c4e96f00f0339417
Philipp Eppelt <philipp88@eppelt.de>
2013-01-24 16:51:48 +0100

    algorithm thought

--- src/grouper/__init__.py
+++ src/grouper/__init__.py
@@ -1,18 +1,32 @@
+#!/bin/python
 
-
-
-
-
-
+from pymongo import MongoClient
 
 class Grouper:
   first = True
+  db
 
   def init(self):
+    connection = MongoClient()
+    self.db = connection.complexlab_ai
     # open DB connection
     # test connection to DB
   
   def grouping(self):
+# databsae layout:
+#   Characteristic: ID,{ID}
+
+  # Options:
+  # 1. generate ID: {Characteristic} - table and check for equal characts
+  # 2. build charact-set for first ID, check own characteristics for equal
+    # IDs and save (non-)equality in a matrix
+  # 3. gerate all Characteristic combinations and intersect their ID-Sets
+      # delete empty characteristic combinations?
+      # -> approach is complete
+      # -> works directly on database layout
+      # -> simple
+
+#  ---------------------------
   # while there is data:
   # aquire dataset from the DB 
   # for data in dataset: extract the featureset
