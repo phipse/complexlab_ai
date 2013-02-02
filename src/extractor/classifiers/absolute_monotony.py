3fa9ca1b24da710353c82f4ac367197e54d21199
Peter Schwede <Peter.Schwede@gmx.net>
2013-02-02 17:11:07 +0100

    absolute and relative values for monotony

---
+++ src/extractor/classifiers/absolute_monotony.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+
+from classifier import Classifier
+
+class AbsoluteIncreasing(Classifier):
+    name = "increasing"
+    can_overlay = False
+
+    def can_start(self, time, value):
+        self.value = value
+        return True
+
+    def has_to_end(self, time, value):
+        jupp = value < self.value
+        if jupp:
+            self.value = value - self.value
+        return jupp
+
+
+class AbsoluteDecreasing(AbsoluteIncreasing):
+    name = "decreasing"
+
+    def has_to_end(self, time, value):
+        jupp = value > self.value
+        if jupp:
+            self.value = self.value - value
+        return jupp
