43aeef6eff24899cfec56b5e3a16e2fdaf33b4c4
Marius Melzer <marius.melzer@rasumi.net>
2013-02-11 18:44:59 +0100

    new summarist, with: access to mongodb, saving of features, updating of meta object still without: aggregation of characteristics (TBD)

--- src/summarist/__init__.py
+++ src/summarist/__init__.py
@@ -1,13 +1,30 @@
-from pymongo import MongoClient
+from pymongo import Connection
 
 class Summarist:
     def __init__(self):
-        connection = MongoClient()
-        self.db = connection.complexlab_ai
+        connection = Connection()
+        self.db = connection.ai
 
     def process(self, id, features):
         for feature in features:
-            cs = Characteristic.load(self.db, feature.name)
-            fits = [c for c in cs if c.similarity_to(feature) > 0.9]
-            for fit in fits: c.insert(feature)
-            db.characteristics.save(fits)
+            db.features.insert({"name": feature.name, "id": feature.id}) # TODO ensureIndex(name, id)
+            if db.features.count % 1000:
+                # update meta-object
+                meta = db.meta.find_one({"_id": feature.name})
+                default_id_ranges = feature.__class__.default_id_ranges
+                if(meta == None):
+                    meta = {"_id": feature.name, "id_ranges" : default_id_ranges} # using _id because it is indexed by default
+                for i in range(0, len(feature.id)-1):
+                    if(default_id_ranges[i] == None):
+                        # update meta.ranges[i]
+                        if meta.id_ranges[i] == None:
+                            meta.id_ranges[i] == {"min": feature.id[i], "max": feature.id[i]}
+                        else:
+                            if feature.id[i] < meta.id_ranges[i]:
+                                meta.id_ranges[i].min = feature.id[i]
+                            elif feature.id[i] > meta.id_ranges[i]:
+                                meta.id_ranges[i].max = feature.id[i]
+                db.meta.save(meta)
+
+                # aggregate to characteristics, if possible async
+                # merge characteristics, if possible async. lock!
