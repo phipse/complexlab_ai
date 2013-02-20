12eba5a77e1a407bc98ade372e17e2f18236167b
Marius Melzer <marius.melzer@rasumi.net>
2013-02-20 18:26:20 +0100

    change id -> attributes, default_id_ranges -> default_attr_ranges, id_ranges -> attr_ranges in summarist

--- src/summarist/__init__.py
+++ src/summarist/__init__.py
@@ -5,23 +5,23 @@ class Summarist:
         connection = Connection()
         self.db = connection.ai
 
-    def process(self, id, features):
+    def process(self, features):
         for feature in features:
-            db.features.insert({"name": feature.name, "id": feature.id}) # TODO ensureIndex(name, id)
+            db.features.insert({"name": feature.name, "attributes": feature.attributes}) # TODO ensureIndex(name, id)
             if db.features.count % 1000:
                 # update meta-object
                 meta = db.meta.find_one({"_id": feature.name})
-                default_id_ranges = feature.__class__.default_id_ranges
+                default_attr_ranges = feature.default_attr_ranges
                 if(meta == None):
-                    meta = {"_id": feature.name, "id_ranges" : default_id_ranges} # using _id because it is indexed by default
-                for i in range(0, len(feature.id)-1):
-                    if meta.id_ranges[i] == None: # create meta object for feature-name
-                        meta.id_ranges[i] == [feature.id[i], feature.id[i]] # [min, max]
-                    elif(default_id_ranges[i] == None): # only update meta object if not specified by user (via feature-config)
-                        if feature.id[i] < meta.id_ranges[i]: # update min?
-                            meta.id_ranges[i][0] = feature.id[i]
-                        elif feature.id[i] > meta.id_ranges[i]: # update max?
-                            meta.id_ranges[i][1] = feature.id[i]
+                    meta = {"_id": feature.name, "attr_ranges" : default_attr_ranges} # using _id because it is indexed by default
+                for i in range(0, len(feature.attributes)-1):
+                    if meta.attr_ranges[i] == None: # create meta object for feature-name
+                        meta.attr_ranges[i] == [feature.attributes[i], feature.attributes[i]] # [min, max]
+                    elif(default_attr_ranges[i] == None): # only update meta object if not specified by user (via feature-config)
+                        if feature.attributes[i] < meta.attr_ranges[i]: # update min?
+                            meta.attr_ranges[i][0] = feature.attributes[i]
+                        elif feature.attributes[i] > meta.attr_ranges[i]: # update max?
+                            meta.attr_ranges[i][1] = feature.attributes[i]
                 db.meta.save(meta)
 
                 # aggregate to characteristics, if possible async
