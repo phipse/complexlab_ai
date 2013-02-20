7a23c868cc307845219ef16c4ea4963c124ba539
Marius Melzer <marius.melzer@rasumi.net>
2013-02-20 18:25:33 +0100

    divide feature to feature and feature_class

--- src/features/__init__.py
+++ src/features/__init__.py
@@ -1,17 +1,14 @@
-class Feature:
-    name = None # name of the feature, e.g. "Monotony"
+__all__ = [feature_class]
 
-    @classmethod
-    def initialize(cls, default_id_ranges, opts = {}):
-        if not isinstance(default_id_ranges, list): raise TypeError, "default_id_ranges must be a list"
-        cls.default_id_ranges = default_id_ranges
+class Feature:
+    def __init__(attributes, feature_class):
+        self.attributes = attributes
+        self.feature_class = feature_class
 
     @property
-    def id(self):
-        return self._id
+    def name(self):
+        self.feature_class.name
 
-    @id.setter
-    def id(self, id):
-        if not isinstance(id, list) or len(default_id_ranges) != len(id):
-            raise ValueError, "id must be a list and have the same length like default_id_ranges"
-        self._id = id
+    @property
+    def default_attr_ranges:
+        self.feature_class.default_attr_ranges
