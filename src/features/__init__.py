cf5eea39da72b1be01c55e074e084979c763c44d
Marius Melzer <marius.melzer@rasumi.net>
2013-02-28 15:37:44 +0100

    fix 2 syntax errors in features/__init__.py

--- src/features/__init__.py
+++ src/features/__init__.py
@@ -1,4 +1,4 @@
-__all__ = [feature_class]
+import feature_class
 
 class Feature:
     def __init__(attributes, feature_class):
@@ -10,5 +10,5 @@ class Feature:
         self.feature_class.name
 
     @property
-    def default_attr_ranges:
+    def default_attr_ranges(self):
         self.feature_class.default_attr_ranges
