e36b4053398e96156a68b10b397c7f9624351fde
Philipp Eppelt <philipp88@eppelt.de>
2013-03-05 14:44:35 +0100

    init refactored according to extractor.init and import fixes

--- src/features/__init__.py
+++ src/features/__init__.py
@@ -1,14 +1,2 @@
-import feature_class as FeatureClass
-
-class Feature:
-    def __init__(attributes, feature_class):
-        self.attributes = attributes
-        self.feature_class = feature_class
-
-    @property
-    def name(self):
-        self.feature_class.name
-
-    @property
-    def default_attr_ranges(self):
-        self.feature_class.default_attr_ranges
+from feature_class import FeatureClass
+from feature import Feature
