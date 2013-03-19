0174568f18c0e76ab92e3793d82f434ae7617ad1
Marius Melzer <marius.melzer@rasumi.net>
2013-03-19 14:44:16 +0100

    delete properties name and default_attr_ranges from feature, now only accessible via feature_group

--- src/features/feature.py
+++ src/features/feature.py
@@ -2,11 +2,3 @@ class Feature:
     def __init__(self, attributes, feature_group):
         self.attributes = attributes
         self.feature_group = feature_group
-
-    @property
-    def name(self):
-        self.feature_group.name
-
-    @property
-    def default_attr_ranges(self):
-        self.feature_class.default_attr_ranges
