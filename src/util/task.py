e85a24b62e4dcdb4bb2ae25f82623983f08395da
Marius Melzer <marius.melzer@rasumi.net>
2013-04-08 12:10:53 +0200

    add merge-threshold to test_task and parse it in utils/task.py

--- src/util/task.py
+++ src/util/task.py
@@ -4,29 +4,32 @@ import logging
 class Task(object):
     def check_api(self, api):
         if not 'name' in api:
-            raise SyntaxError("Error parsing config: api without a 'name'")
+            raise SyntaxError("Error parsing task: api without a 'name'")
         if not 'features_to_observe' in api:
-            raise SyntaxError("Error parsing config: no 'features_to_observe' in API '%s' specfied" % api['name'])
+            raise SyntaxError("Error parsing task: no 'features_to_observe' in API '%s' specfied" % api['name'])
 
     def check_fto(self, api, fto):
         if not 'type' in fto:
             raise SyntaxError("feature_to_observe without a type in API '%s'" % api['name'])
         if not 'default_attr_ranges' in fto:
-            raise SyntaxError("Error parsing config: no 'default_attr_ranges' in feature '%s' specified" % fto['type'])
+            raise SyntaxError("Error parsing task: no 'default_attr_ranges' in feature '%s' specified" % fto['type'])
+        if not 'merge_threshold' in fto:
+            raise SyntaxError("Error parsing task: no 'merge_threshold' in feature '%s' specified" % fto['type'])
         if not 'options' in fto:
-            raise SyntaxError("Error parsing config: no 'options' in feature '%s' specified" % fto['type'])
+            raise SyntaxError("Error parsing task: no 'options' in feature '%s' specified" % fto['type'])
 
     def __init__(self, path):
         self.mask_names = []
         self.default_attr_ranges = {}
+        self.merge_threshold = {}
         logging.debug("Loading json %s", path)
         task = json.load(open(path, "r"))
         if 'apis' not in task:
-            raise SyntaxError("Error parsing config: no 'apis' specfied")
+            raise SyntaxError("Error parsing task: no 'apis' specfied")
         for api in task['apis']:
             self.check_api(api)
             for fto in api['features_to_observe']:
                 self.check_fto(api, fto)
                 self.mask_names.append(fto["type"].strip())
                 self.default_attr_ranges[fto["type"].strip()] = fto["default_attr_ranges"]
-
+                self.merge_threshold[fto["type"].strip()] = fto["merge_threshold"]
